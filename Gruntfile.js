4b8f13fba0241b8885673b9ea33b372aec43eb3f
ovchar-dmitriy <ovchar.dmitriy@gmail.com>
2015-11-05 16:28:00 +0200

    add lazy loading for carousels

--- Gruntfile.js
+++ Gruntfile.js
@@ -248,9 +248,9 @@ module.exports = function (grunt) {
 
         // Performs rewrites based on filerev and the useminPrepare configuration
         usemin: {
-            html: ['<%= yeoman.dist %>/{,*/}*.html'],
-            css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
-            js: ['<%= yeoman.dist %>/scripts/{,*/}*.js'],
+            html: ['<%= yeoman.dist %>/**/*.html'],
+            css: ['<%= yeoman.dist %>/styles/**/*.css'],
+            js: ['<%= yeoman.dist %>/scripts/**/*.js'],
             options: {
                 assetsDirs: [
                     '<%= yeoman.dist %>',
--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -9,7 +9,38 @@
  */
 
 angular.module("yoPortfolio")
-    .controller("ProjectCtrl", function ($rootScope, $scope, $stateParams, projects) {
+    .controller("ProjectCtrl", function ($rootScope, $scope, $stateParams, projects) {  
+
+    // setting up img's lazy loading
+    $rootScope.cHeight = 0;
+    
+    if ($rootScope.carousel$ !== undefined) {
+        $rootScope.carousel$.off("slide.bs.carousel");
+    }
+    
+    $rootScope.carousel$ = $("#carousel");    
+
+    $rootScope.carousel$.on("slide.bs.carousel", function(e) {
+    
+        var $nextImage = $(e.relatedTarget).find("img"),        
+            $activeItem = $(".active.item", this),
+            src;
+        
+        // prevents the slide decrease in height
+        if ($rootScope.cHeight == 0) {
+            $rootScope.cHeight = $(this).height();
+            $activeItem.next(".item").height($rootScope.cHeight);
+        }
+        
+        // prevents the loaded image if it is already loaded
+        src = $nextImage.data("lazy-load-src");
+        
+        if (typeof src !== "undefined" && src != "") {
+            $nextImage.attr("src", src)
+            $nextImage.data("lazy-load-src", "");
+        }
+    });
+    
     $scope.project = projects.getProjectByPath($stateParams.id);
     
     $rootScope.pageTitle = $scope.project.title;
